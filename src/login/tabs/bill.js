1e9ff46a688f827df390ece7392163bcd8b174a6
王逸然 <hotpotman@wangyirandeMacBook-Air.local>
2018-11-19 20:17:34 +0800

    newicon

--- src/login/tabs/bill.js
+++ src/login/tabs/bill.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { List } from 'antd-mobile';
+
+const Item = List.Item;
+const Brief = Item.Brief;
 
 export default class BillPage extends Component {
   render() {
-    return <div>
-      Bill Page11111
-    </div>
+    return (<div>
+        <List  className="my-list">
+            <Item>账单</Item>
+            <Item extra="-110" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
+                张三 <Brief>2018-10-31</Brief>
+            </Item>
+            <Item extra="+120" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
+                李四 <Brief>2018-11-01</Brief>
+            </Item>
+            <Item extra="-50" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
+                王五 <Brief>2018-12-01</Brief>
+            </Item>
+        </List>
+    </div>);
   }
 }
